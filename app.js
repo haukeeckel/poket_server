dcc0320666527a6110fade5b1a71b208a77e3f22
haukeeckel <Haukeeckel@posteo.de>
2021-12-14 11:55:36 +0100

    fix: static path

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ app.use(
   })
 );
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 const allRoutes = require('./routes');
 app.use('/api', allRoutes);
 
